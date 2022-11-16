0f7bceaaa0e0eb1f97f8006c765439d937873ff3
abisi <axel.bisi@epfl.ch>
2022-11-16 10:53:23 +0100

    reformat log lick data

--- log_lick_data.m
+++ log_lick_data.m
@@ -1,38 +1,44 @@
-function log_lick_data(~, evt, TrialDuration)
+function log_lick_data(~, event, trial_duration)
 % LOG_LICK_DATA Save trial lick data in .bin files and plots lick data for
 % GUI.
 % EVT
 % TRIALDURATION Duration of current trial.
 
-global Stim_S fid3 handles2give Lick_Threshold Stim_S_SR TrialLickData
+global Stim_S fid3 handles2give lick_threshold Stim_S_SR trial_lick_data
 
-%% Get lick data and write downsample in .bin files
-[data_stim, timestamps, trigger_time] = read(Stim_S, Stim_S.ScansAvailableFcnCount,'OutputFormat','Matrix');
+%% Get lick data and write downsample data in .bin files
+%[data_stim, timestamps, trigger_time] = read(Stim_S, Stim_S.ScansAvailableFcnCount,'OutputFormat','Matrix');
 
-data = [timestamps(:,1), data_stim(:,:)]';
+data = [event.TimeStamps(:,1), event.Data(:,:)]' ;
 fwrite(fid3, downsample(data, 10),'double');
 
 %% Plotting lick data
 % Plot progress bar 
-plot(handles2give.ProgressBarAxes, [timestamps(end) timestamps(end)], [0 1], 'LineWidth',4, 'Color','w')
+plot(handles2give.ProgressBarAxes, [event.TimeStamps(end) event.TimeStamps(end)], [0 1], 'LineWidth', 4, 'Color', 'w')
 set(handles2give.ProgressBarAxes, 'Color',[0.4 0.4 0.4]);
-xlim(handles2give.ProgressBarAxes, [0 TrialDuration/1000])
+xlim(handles2give.ProgressBarAxes, [0 trial_duration/1000])
 set(handles2give.ProgressBarAxes, 'XTick',[], 'XColor','w', ...
                                    'YTick',[], 'YColor','w', ...
                                    'Box','off')
 
-% Plot trial lick data
-TrialLickData = [TrialLickData, abs(data(2,:))];
+% Piezo sensor data as absolute signals
+trial_lick_data = [trial_lick_data, abs(data(2,:))];
 
-timevec=linspace(0,numel(TrialLickData)/Stim_S_SR,numel(TrialLickData)); %X-axis time vector
-plot(handles2give.LickTraceAxes2,timevec,TrialLickData,'Color','k'); %Licking trace
-line([0 TrialDuration/1000],[Lick_Threshold Lick_Threshold],'LineWidth',1,'LineStyle','--',... % Licking threshold line
-    'Color','r','Parent', handles2give.LickTraceAxes2);
+% Plot trial lick data
+timevec=linspace(0, numel(trial_lick_data)/Stim_S_SR, numel(trial_lick_data)); %x-axis time vector
+plot(handles2give.LickTraceAxes2,timevec,trial_lick_data,'Color','k'); %licking trace
+
+% Show lick detection threshold line
+line([0 trial_duration/1000], [lick_threshold lick_threshold], ...
+    'LineWidth', 1, ...
+    'LineStyle', '--', ... 
+    'Color', 'r', ...
+    'Parent', handles2give.LickTraceAxes2);
 
 % Set axes limits
-ylim(handles2give.LickTraceAxes2,[0 Lick_Threshold*5]);
-xlim(handles2give.LickTraceAxes2,[0 TrialDuration/1000]);
-set(handles2give.LickTraceAxes2,'XTick',[])
+ylim(handles2give.LickTraceAxes2, [0 lick_threshold*5]);
+xlim(handles2give.LickTraceAxes2, [0 trial_duration/1000]);
+set(handles2give.LickTraceAxes2, 'XTick', [])
 
 
 
