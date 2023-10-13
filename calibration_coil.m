3a7615683e9756ae6cb349ade86f27e740a40d82
abisi <axel.bisi@epfl.ch>
2023-10-13 11:03:41 +0200

    update stim_array generation for multiple stim amps, save all stim amps per trials, save plot, update save_folder

--- calibration_coil.m
+++ calibration_coil.m
@@ -22,9 +22,12 @@ dest_path = 'C:\Users\bisi\Desktop\Calibration';
 mouse_name = 'ABXXX';
 date = datetime('today', 'Format', 'yyyyMMdd');
 
-% Create mouse and calibration directory 
+% Create mouse directory and calibration subdirectories 
 if ~exist([char(dest_path) '\' char(mouse_name)], 'dir')
     mkdir(dest_path,mouse_name);
+end
+if ~exist([char(dest_path) '\' char(mouse_name) '\'  char(mouse_name) '_' char(date)],'dir')
+    mkdir([char(dest_path) '\' char(mouse_name)], [char(mouse_name) '_' char(date)]);
 end
 
 
@@ -115,6 +118,8 @@ elseif strcmp(stim_name,'biphasic_hann_3ms')
 elseif strcmp(stim_name, 'biphasic_hann_3ms_psy')
 
     stim_amp_volt_max = 2.8;
+
+    % Define voltages to test
     stim_amp_volt_list = [
         stim_amp_volt_max;
         stim_amp_volt_max * 0.8;
@@ -135,6 +140,7 @@ elseif strcmp(stim_name, 'biphasic_hann_3ms_psy')
     impulse_down = impulse_down(2:end);
     impulse = [impulse_up' scale_factor*impulse_down'];
 
+
     stim_array = zeros(length(stim_amp_volt_list), sr*trial_dur);
     for istim=1:length(stim_amp_volt_list)
         stim_amp = stim_amp_volt_list(istim);
@@ -142,36 +148,57 @@ elseif strcmp(stim_name, 'biphasic_hann_3ms_psy')
         stim = [biphasic_hann_3ms zeros(1,trial_dur*sr - numel(biphasic_hann_3ms))];
         stim_array(istim,:) = stim';
     end
-
 end
 
 
 %% Test stimulus, plot and save calibration data.
 
-n_trials = 2;
-len = numel(stim);
-flux_data = zeros(n_trials,len);
+n_trials = 3; % number of trials to test, per stimulus
+len = numel(stim); % number of data points in stimulus
 
 if strcmp(stim_name,'biphasic_hann_3ms_psy')
-    n_stim_amp = height(stim_array);
+
+    % Tile different stimulus amplitudes, and stimuli
+
+    trials_stim_amp_volt = [ ...
+    repmat(stim_amp_volt_list(1),n_trials,1); ...    
+    repmat(stim_amp_volt_list(2),n_trials,1); ...    
+    repmat(stim_amp_volt_list(3),n_trials,1); ...    
+    repmat(stim_amp_volt_list(4),n_trials,1); ...    
+    ];
+
+    stim_array = [ ...
+    repmat(stim_array(1,:),n_trials,1); ...
+    repmat(stim_array(2,:),n_trials,1); ...
+    repmat(stim_array(3,:),n_trials,1); ...
+    repmat(stim_array(4,:),n_trials,1); ...
+    ];
+
+    % Update number of trials to send; init. data storage
+    n_stim_amp = height(stim_amp_volt_list);
     n_trials = n_trials * n_stim_amp;
     flux_data = zeros(n_trials,len);
+else
+
+    flux_data = zeros(n_trials,len);
+    trials_stim_amp_volt = repmat(stim_amp_volt, n_trials, 1);
+
 end
     
     
 disp(['Starting calibration: ' stim_name]);
+
 for itrial=1:n_trials
    disp(['Stim ' int2str(itrial)]);
 
    if strcmp(stim_name,'biphasic_hann_3ms_psy')
-        %stim = stim_array(itrial,:);
-        stim = stim_array(1+mod(itrial-1, n_stim_amp),:);
+        stim = stim_array(itrial,:);
    end
 
    queueOutputData(session, stim');
    prepare(session);
    d = startForeground(session);
-   flux_data(itrial,:) = d(:,1);
+   flux_data(itrial,:) = d(:,1); % keep data
    pause(.5);
 
 end
@@ -182,18 +209,21 @@ session.release();
 disp('Calibration done.')
 
 
-% Find mean amplitude maximum of magnetic flux in mT findpeaks
+% Find maximum amplitude of magnetic flux in mT 
 
-min_prominence = 0.05; %in mT
-stim_amp_millitesla = zeros(n_trials,0);
-stim_amp_index = zeros(n_trials,0);
+min_prominence = 0.05; % teslameter scale: 5 mT
+trials_stim_amp_millitesla = zeros(n_trials,0);
+trials_stim_amp_index = zeros(n_trials,0);
 
 for itrial=1:n_trials
     [pks,locs] = findpeaks(flux_data(itrial,:),'Npeaks',1,'MinPeakProminence',min_prominence);
-    stim_amp_millitesla(itrial) = pks;
-    stim_amp_index(itrial) = locs;
+    trials_stim_amp_millitesla(itrial) = pks * 100; % teslameter scale
+    trials_stim_amp_index(itrial) = locs;
 end
-mean_amp_millitesla = mean(stim_amp_millitesla) * 100;  % teslameter scale 
+
+% Get mean amplitude across trials (for single-amplitude stimuli)
+
+mean_amp_millitesla = mean(trials_stim_amp_millitesla) * 100;  % teslameter scale 
 
 % Plot stimulus vector and magnetic flux 
 
@@ -213,7 +243,6 @@ ylabel(ax2, 'Magnetic flux [mT]');
 
 linkaxes([ax1,ax2],'x');
 
-%findpeaks(flux_data(1,:),time,'Npeaks',1,'MinPeakProminence',min_prominence,'Annotate','extents')
 xlim([1995 2015])
 xlabel('Time [ms]')
 ylabel(ax1, 'Impulse [V]');
@@ -221,9 +250,9 @@ ylabel(ax2, 'Magnetic flux [mT]');
 
 
 % Save figure and calibration data in file
-
+save_folder = [char(dest_path) '\' char(mouse_name) '\'  char(mouse_name) '_' char(date)];
 figure_file = [mouse_name '_' int2str(yyyymmdd(date)) '_stim_coil_calibration'];
-figure_file = fullfile([char(dest_path) '\' char(mouse_name)], figure_file);
+figure_file = fullfile(save_folder, figure_file);
 saveas(gcf, figure_file, 'png')
 
 
@@ -232,13 +261,7 @@ if strcmp(stim_name,'biphasic_hann_3ms_psy')
 end
 
 data_file = [mouse_name '_' int2str(yyyymmdd(date)) '_stim_coil_calibration.m'];
-data_file = fullfile([char(dest_path) '\' char(mouse_name)], data_file);
-save(data_file,'mouse_name','stim_name', 'n_trials','flux_data','stim','stim_amp_volt','mean_amp_millitesla', 'stim_amp_millitesla', 'stim_duration_up','stim_duration_down','scale_factor','session_sampling_rate')
+data_file = fullfile(save_folder, data_file);
+save(data_file,'mouse_name','stim_name','n_trials','flux_data','stim','trials_stim_amp_volt','trials_stim_amp_millitesla','mean_amp_millitesla','stim_duration_up','stim_duration_down','scale_factor','session_sampling_rate')
 disp(['Calibration data saved in: ' data_file])
-
-
-
-
-
-
 
